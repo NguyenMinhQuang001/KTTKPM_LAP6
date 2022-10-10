1b0c0ad719c2481e104baec6f22632acce28bb30
NguyenMinhQuang001 <quangnguyenminh2601@gmail.com>
2022-10-10 08:43:23 +0700

    First Commit

---
+++ Demo-Quanlychuyenbay-SB-AMQ/src/main/java/com/example/DemoQuanlychuyenbaySBAMQ/DemoQuanlychuyenbaySbAmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoQuanlychuyenbaySBAMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQuanlychuyenbaySbAmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoQuanlychuyenbaySbAmqApplication.class, args);
+	}
+
+}
---
+++ Demo-Quanlychuyenbay-SB-AMQ/src/test/java/com/example/DemoQuanlychuyenbaySBAMQ/DemoQuanlychuyenbaySbAmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoQuanlychuyenbaySBAMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQuanlychuyenbaySbAmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
